4287f2d188fe134046e1d669a529ff63fcfe5130
Adijav <agjavali@gmail.com>
2016-04-03 14:01:37 -0700

    added the bootstrap and react resources to the app

---
+++ script.jsx
@@ -0,0 +1,13 @@
+(function(){
+	'use strict'
+
+	var Quiz = React.createClass({
+		render: function(){
+			return(
+				<div>Quiz App</div>
+			);
+		}
+	});
+
+	ReactDOM.render(<Quiz/>,document.getElementById("container"));
+})();
